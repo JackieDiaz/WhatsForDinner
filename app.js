69561b4e2d63b295f7ae46eddc6917c638df7b21
AJ <alexanderjessediaz@gmail.com>
2020-08-25 15:31:35 -0600

    fix spacing in js files

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(baseURL)
     .then(response => response.json())
     .then(handleDinnerObjectData)
-
-
     function handleDinnerObjectData(dinnerObject){
         console.table(dinnerObject)
         dinnerObject.forEach(recipeOverview => {
@@ -12,11 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
             
         })
     }
-
     const recipeCardContainer = document.querySelector(".recipe-cards-container")
     const newUserRecipe = document.getElementById("#add-new-recipe")
-
-
     newUserRecipe.addEventListener("submit", ()=> {
         event.preventDefault()
         const formData = new FormData(newUserRecipe)
@@ -40,18 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
     })
-    
     function renderRecipeOverviewCard(recipeOverview){
-        console.log(recipeOverview)
         const recipeCard = document.createElement("h3")
         recipeCard.innerHTML = 
-        `
-            <div class="card">
-                <br><a class="card-title" href='recipeOverview.html?id=${recipeOverview.id}'>${recipeOverview.name}</a><br>
-                <br><p>time to cook: hours/minutes ${recipeOverview.cook_time}</p><br> 
-                <br><img ${recipeOverview.image}/><br> 
-            </div>
-        `
+            `
+                <div class="card">
+                    <br><a class="card-title" href='recipeOverview.html?id=${recipeOverview.id}'>${recipeOverview.name}</a><br>
+                    <br><p>time to cook: hours/minutes ${recipeOverview.cook_time}</p><br> 
+                    <br><img ${recipeOverview.image}/><br> 
+                </div>
+            `
         recipeCardContainer.appendChild(recipeCard)
     }
 })
--- recipe_overview.js
+++ recipe_overview.js
@@ -2,11 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchParams = new URLSearchParams(document.location.search)
     const query = searchParams.get("id")
     const baseURL = `http://localhost:4000/recipes/${query}`
-
     fetch(baseURL)
     .then(response => response.json())
     .then(handleRecipeOverviewData)
-    
     function handleRecipeOverviewData(recipeOverview){
         recipeOverview.recipes.map(recipe => {
             renderRecipeCard(recipe)
@@ -14,45 +12,39 @@ document.addEventListener('DOMContentLoaded', () => {
             
         })
     }
-    
     function splitIngredientData(recipe){
         const splitIngredients = recipe.ingredients.split(";")
         splitIngredients.map(ingredient => {
             renderIngredients(ingredient)
         })
     }
-    
     function renderRecipeCard(recipe){
         const recipeCardContainer = document.querySelector(".recipe-cards-container")
         const recipeCard = document.createElement("main")
-        recipeCard.innerHTML = `
-        <div class="card">
-            <br><h1>Title: ${recipe.name}</h1><br>
-            <p>Cook time: ${recipe.cook_time} hours/minutes</p>
-            <img src=${recipe.image}>
-            <br><p>Instructions ${recipe.instructions}</p><br>
-        </div>
-        `
+        recipeCard.innerHTML = 
+            `
+                <div class="card">
+                    <br><h1>Title: ${recipe.name}</h1><br>
+                    <p>Cook time: ${recipe.cook_time} hours/minutes</p>
+                    <img src=${recipe.image}>
+                    <br><p>Instructions ${recipe.instructions}</p><br>
+                </div>
+            `
         recipeCardContainer.appendChild(recipeCard)   
     }
-    
     function renderIngredients(ingredient){
         const ingredientsContainer = document.querySelector(".ingredients-container")
         const ingredientElement = document.createElement("li")
         ingredientElement.textContent = ingredient
         ingredientsContainer.appendChild(ingredientElement)
         createAddIngredientBtn(ingredientElement, ingredient)
-    
     }
-    
-    
     function createAddIngredientBtn(ingredientElement){
         const addIngredientBtn = document.createElement("button")
         addIngredientBtn.textContent = "add to grocery list"
         ingredientElement.append(addIngredientBtn)
         addIngredientBtn.addEventListener("click", () => addIngredientToGroceryList(ingredientElement))
     }
-    
     function addIngredientToGroceryList(ingredientElement){
         const groceryItem = document.createElement("li")
         const groceryList = document.querySelector("#grocery-list")
@@ -61,56 +53,5 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault()
         groceryList.appendChild(groceryItem)
     }
-            
-        })
 
-        // const word = 'pneumonoultramicroscopicsilicovolcanoconiosis'
-
-// function getObjFromStr(word){
-// 	const wordObj = {};
-// 	// take in each character from word and assign the key to be the char
-// 	// value will be the number of times the char occured
-// 	// break word into array then iterate through array
-// 	// forEach 
-
-// 	word.split('').forEach(char => {
-		
-// 	})
-// }
-
-// const buttonComponent = () => {
-	
-// 	increment = () => {
-// 		const num = 0
-// 		return num++
-// 	}
-
-// 	return(
-
-// 	<div>
-// 		<button onCLick>
-			
-// 		</button>
-
-// 	</div>
-
-// 	)
-// }
-
-
-// console.log(getObjFromStr(word))
-
-// /*
-// {
-// 	p: 2,
-// 	n: 4,
-// 	e: 1,
-// 	...
-// }
-
-// wordObj["p"] // 2
-// */
-
-// strong fundamentals
-// explain code clearly
-// 
+})
